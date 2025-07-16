1699977babc0148f503d3786592024a74cf0591e
malaonda222 <bandinelli.lisa@gmail.com>
2025-07-16 16:08:45 +0200

    commit

--- Esempi/VoliAerei/voliaerei3.sql
+++ Esempi/VoliAerei/voliaerei3.sql
@@ -4,6 +4,40 @@ create domain PosInteger1900 as integer check (value > 1900);
 create domain DurataMinuti as integer check (value > 0);
 
 
+create table Nazione (
+    id PosInteger not null,
+    nome Stringa not null,
+    primary key (id)
+);
+
+create table Citta (
+    id PosInteger not null,
+    nome Stringa not null,
+    abitanti PosInteger not null,
+    nazione PosInteger not null,
+    primary key (id),
+    foreign key (nazione) references Nazione(id)
+);
+
+create table CompagniaAerea (
+    id PosInteger not null, 
+    nome Stringa not null,
+    fondazione PosInteger1900 not null,
+    citta PosInteger not null,
+    primary key (id), 
+    foreign key (citta) references Citta(id)
+);
+
+create table Aeroporto (
+    id PosInteger not null,
+    nome Stringa not null,
+    codice Stringa not null,
+    citta PosInteger not null, 
+    primary key (id),
+    unique (codice),
+    foreign key (citta) references Citta(id)
+);
+
 create table Volo (
     id PosInteger not null,
     codice Stringa not null,
@@ -26,37 +60,3 @@ create table Volo (
 --     foreign key (aeroporto) references Aeroporto(id),
 --     foreign key (volo) references Volo(id) deferrable
 -- );
-
-create table Aeroporto (
-    id PosInteger not null,
-    nome Stringa not null,
-    codice Stringa not null,
-    citta PosInteger not null, 
-    primary key (id),
-    unique (codice),
-    foreign key (citta) references Citta(id)
-);
-
-create table CompagniaAerea (
-    id PosInteger not null, 
-    nome Stringa not null,
-    fondazione PosInteger1900 not null,
-    citta PosInteger not null,
-    primary key (id), 
-    foreign key (citta) references Citta(id)
-);
-
-create table Citta (
-    id PosInteger not null,
-    nome Stringa not null,
-    abitanti PosInteger not null,
-    nazione PosInteger not null,
-    primary key (id),
-    foreign key (nazione) references Nazione(id)
-);
-
-create table Nazione (
-    id PosInteger not null,
-    nome Stringa not null,
-    primary key (id)
-);
