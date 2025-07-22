907f35bb6900a6f8d37c21645175d6aba71b75f8
malaonda222 <bandinelli.lisa@gmail.com>
2025-07-22 16:49:22 +0200

    commit

--- Esempi/Cielo/cielo1.sql
+++ Esempi/Cielo/cielo1.sql
@@ -25,22 +25,19 @@ from arrpart
 where arrivo = 'FCO'
 
 
-/*5. Quali sono i voli (codice e nome della compagnia) che partono dall’aeroporto ‘FCO’ e 
-arrivano all’aeroporto ‘JFK’?*/
+/*5. Quali sono i voli (codice e nome della compagnia) che partono dall’aeroporto ‘FCO’ e  arrivano all’aeropor ‘JFK’?*/
 select codice, comp
 from arrpart 
 where partenza = 'FCO' and arrivo = 'JFK'
 
 
-/*6. Quali sono le compagnie che hanno voli che partono dall’aeroporto ‘FCO’ e atterrano 
-all’aeroporto ‘JFK’?*/
+/*6. Quali sono le compagnie che hanno voli che partono dall’aeroporto ‘FCO’ e atterrano all’aeroporto ‘JFK’?*/
 select distinct comp
 from arrpart 
 where partenza = 'FCO' and arrivo = 'JFK'
 
 
-/*7. Quali sono i nomi delle compagnie che hanno voli diretti dalla città di ‘Roma’ alla
-città di ‘New York’?*/
+/*7. Quali sono i nomi delle compagnie che hanno voli diretti dalla città di ‘Roma’ alla città di ‘New York’?*/
 select distinct comp
 from arrpart a 
 	join luogoaeroporto l1 on a.partenza = l1.aeroporto
@@ -48,8 +45,7 @@ from arrpart a
 where l1.citta = 'Roma' and l2.citta = 'New York'
 
 
-/*8. Quali sono gli aeroporti (con codice IATA, nome e luogo) nei quali partono voli della compagnia 
-di nome ‘MagicFly’?*/
+/*8. Quali sono gli aeroporti (con codice IATA, nome e luogo) nei quali partono voli della compagnia di nome ‘MagicFly’?*/
 select a.codice, a.nome, l.citta
 from arrpart ar
     join luogoaeroporto l on ar.partenza = l.aeroporto
@@ -57,9 +53,7 @@ from arrpart ar
 where ar.comp = 'MagicFly'
 
 
-/*9. Quali sono i voli che partono da un qualunque aeroporto della città di ‘Roma’ e 
-atterrano ad un qualunque aeroporto della città di ‘New York’? 
-Restituire: codice del volo, nome della compagnia, e aeroporti di partenza e arrivo.*/
+/*9. Quali sono i voli che partono da un qualunque aeroporto della città di ‘Roma’ e atterrano ad un qualunque aeroporto della città di ‘New York’? Restituire: codice del volo, nome della compagnia, e aeroporti di partenza e arrivo.*/
 select ar.codice, ar.comp, ar.partenza, ar.arrivo
 from arrpart ar
     join luogoaeroporto l1 on ar.partenza = l1.aeroporto
@@ -67,8 +61,7 @@ from arrpart ar
 where l1.citta = 'Roma' and l2.citta = 'New York'
 
 
-/*10. Quali sono i possibili piani di volo con esattamente un cambio (utilizzando solo voli della stessa compagnia) da un qualunque aeroporto della città di ‘Roma’ ad un qualunque aeroporto della città di ‘New York’? 
-Restituire: nome della compagnia, codici dei voli e aeroporti di partenza, scalo e arrivo.*/
+/*10. Quali sono i possibili piani di volo con esattamente un cambio (utilizzando solo voli della stessa compagnia da un qualunque aeroporto della città di ‘Roma’ ad un qualunque aeroporto della città di ‘New York’? Restituire: nome della compagnia, codici dei voli e aeroporti di partenza, scalo e arrivo.*/
 select ar1.comp, ar1.codice, l1.aeroporto, l2.aeroporto, l3.aeroporto
 from arrpart ar1
 	join luogoaeroporto l1 on ar1.partenza = l1.aeroporto
@@ -80,8 +73,7 @@ where l1.citta = 'Roma' and l3.citta = 'New York'
 	and ar1.comp = ar2.comp
 
 
-/*11. Quali sono le compagnie che hanno voli che partono dall’aeroporto ‘FCO’, atterrano 
-all’aeroporto ‘JFK' e di cui si conosce l’anno di fondazione?*/
+/*11. Quali sono le compagnie che hanno voli che partono dall’aeroporto ‘FCO’, atterrano all’aeroporto ‘JFK' e di cui si conosce l’anno di fondazione?*/
 select distinct c.nome
 from arrpart ar, compagnia c
 where (ar.partenza = 'FCO' and ar.arrivo = 'JFK') 
