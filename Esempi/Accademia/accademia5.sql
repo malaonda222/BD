3e0e912c7abe0b1b9d94efce58995fb73abcdf46
malaonda222 <bandinelli.lisa@gmail.com>
2025-07-23 21:02:19 +0200

    commit

--- Esempi/Accademia/accademia5.sql
+++ Esempi/Accademia/accademia5.sql
@@ -60,6 +60,13 @@ where a.tipo = 'Malattia' and p.posizione = 'Professore Ordinario'
 group by p.nome, p.cognome, p.posizione
 having count (a.tipo) >= 1
 
+---oppure 
+select distinct p.nome, p.cognome, p.posizione
+from persona p, assenza a 
+where p.id = a.persona 
+	and p.posizione = 'Professore Ordinario'
+	and a.tipo = 'Malattia'
+
 
 /*5. Quali sono il nome, il cognome e la posizione dei Professori Ordinari che hanno
 fatto più di una assenza per malattia?*/
@@ -69,6 +76,15 @@ join assenza a on p.id = a.persona
 where a.tipo = 'Malattia' and p.posizione = 'Professore Ordinario'
 group by p.nome, p.cognome, p.posizione
 having count (a.tipo) > 1
+
+-- oppure 
+select distinct p.nome, p.cognome, p.posizione
+from persona p, assenza a1, assenza a2
+where p.id = a1.persona
+	and p.id = a2.persona
+	and p.posizione = 'Professore Ordinario'
+	and a1.tipo = 'Malattia'
+	and a2.tipo = a1.tipo 
 
 
 /*6. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno almeno
@@ -80,6 +96,13 @@ where anp.tipo = 'Didattica' and p.posizione = 'Ricercatore'
 group by p.nome, p.cognome, p.posizione
 having count (anp.tipo) >= 1
 
+--oppure 
+select distinct p.nome, p.cognome, p.posizione
+from persona p, attivitanonprogettuale anp
+where p.posizione = 'Ricercatore'
+	and p.id = anp.persona 
+	and anp.tipo = 'Didattica'
+
 
 /*7. Quali sono il nome, il cognome e la posizione dei Ricercatori che hanno più di un
 impegno per didattica?*/
@@ -89,6 +112,15 @@ join attivitanonprogettuale anp on p.id = anp.persona
 where anp.tipo = 'Didattica' and p.posizione = 'Ricercatore'
 group by p.nome, p.cognome, p.posizione
 having count (anp.tipo) > 1
+
+-- oppure 
+select distinct p.nome, p.cognome, p.posizione
+from persona p, attivitanonprogettuale anp1, attivitanonprogettuale anp2
+where p.posizione = 'Ricercatore'
+	and p.id = anp1.persona 
+	and p.id = anp2.persona
+	and anp1.tipo = 'Didattica'
+	and anp1.tipo = anp2.tipo
 
 
 /*8. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
@@ -101,11 +133,10 @@ where anp.giorno = ap.giorno
 
 -- oppure 
 select distinct p.nome, p.cognome
-from persona p, attivitaprogetto ap, attivitanonprogettuale anp
-where p.id = ap.persona
-    and p.id = anp.persona 
-    and ap.giorno = anp.giorno 
-
+from persona p, attivitanonprogettuale anp, attivitaprogetto ap
+where p.id = anp.persona 
+	and p.id = ap.persona
+	and anp.giorno = ap.giorno 
 
 /*9. Quali sono il nome e il cognome degli strutturati che nello stesso giorno hanno sia
 attività progettuali che attività non progettuali? Si richiede anche di proiettare il
@@ -118,6 +149,14 @@ join attivitaprogetto ap on p.id = ap.persona
 join progetto prog on ap.progetto = prog.id
 where anp.giorno = ap.giorno
 
+-- oppure
+select p.nome, p.cognome, anp.giorno, prog.nome, anp.tipo, ap.tipo, anp.oredurata, ap.oredurata
+from persona p, attivitanonprogettuale anp, attivitaprogetto ap, progetto prog
+where p.id = anp.persona 
+	and p.id = ap.persona
+	and anp.giorno = ap.giorno 
+	and ap.progetto = prog.id
+
 
 /*10. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
 assenti e hanno attività progettuali?*/
@@ -126,6 +165,13 @@ from persona p
 join assenza a on p.id = a.persona
 join attivitaprogetto ap on p.id = ap.persona 
 where a.giorno = ap.giorno
+
+-- oppure 
+select p.nome, p.cognome
+from persona p, assenza a, attivitaprogetto ap
+where p.id = ap.persona 
+	and p.id = a.persona
+	and ap.giorno = a.giorno
 
 
 /*11. Quali sono il nome e il cognome degli strutturati che nello stesso giorno sono
@@ -137,6 +183,14 @@ join assenza a on p.id = a.persona
 join attivitaprogetto ap on p.id = ap.persona 
 join progetto prog on ap.progetto = prog.id
 where a.giorno = ap.giorno
+
+-- oppure
+select p.nome, p.cognome, ap.giorno, prog.nome, a.tipo, ap.oredurata
+from persona p, assenza a, attivitaprogetto ap
+where p.id = ap.persona 
+	and p.id = a.persona
+	and ap.giorno = a.giorno
+	and ap.progetto = prog.id
 
 
 /*12. Quali sono i WP che hanno lo stesso nome, ma appartengono a progetti diversi?*/
