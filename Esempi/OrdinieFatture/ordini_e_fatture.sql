1b88e40f480ac82efa37e18ec6d8d4fe10b83158
malaonda222 <bandinelli.lisa@gmail.com>
2025-07-28 09:45:53 +0200

    commit

---
+++ Esempi/OrdinieFatture/ordini_e_fatture.sql
@@ -0,0 +1,17 @@
+create type Stato as enum ('in preparazione', 'inviato', 'da saldare', 'saldato')
+
+create domain Stringa as varchar(100)
+
+create domain CodiceFiscale as 
+
+create domain IntGEZ as 
+
+create domain Real_0_1 as  
+
+create domain RealGEZ as
+
+create domain Indirizzo as 
+
+create domain PartitaIVA as  
+
+create domain Email as 
