6a1cbf802a71502184859db9c2aaf9d51bf4a052
malaonda222 <bandinelli.lisa@gmail.com>
2025-09-19 13:03:16 +0200

    commit

--- Esempi/eBuy/eBuy.sql
+++ Esempi/eBuy/eBuy.sql
@@ -1,19 +1,45 @@
+begin transaction;
+
+set constraints all deferred;
+
+create domain StringaM as varchar(100);
+create domain Voto as integer (value between 0 and 5);
+create domain IntGZ1 as integer check (value > 1);
+create domain URL as varchar check (value ~ "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$");
+create domain RealGEZ as real check (value >= 0);
+create domain RealGZ as real check (value > 0);
+create domain PosInteger as integer check (value >= 0);
+create type Condizione as enum ('ottimo', 'buono', 'discreto', 'da sistemare');
+create type Popolarita as enum ('Bassa', 'Media', 'Alta');
+
+
 create table Categoria (
     nome StringaM primary key,
     super StringaM,
     check (nome <> super)
-)
+); 
 
-alter table add foreign key (super) references categoria(nome)
+alter table Categoria add foreign key (super) references Categoria(nome)
 
 create table Utente (
     username StringaM primary key, 
     registrazione timestamp not null,
-)
+);
 
-create table Metodopagamento(
-    nome StringaM primary key
-)
+create table Privato (
+    utente StringaM primary key,
+    foreign key (utente) references Utente(username)
+);
+
+create table VenditoreProfessionale (
+    utente StringaM primary key,
+    vetrina URL not null,
+    unique(vetrina)
+    foreign key (utente) references Utente(username)
+);
+
+
+-- [V.utente.compl] e [V.Utente.disj] non ancora implementati 
 
 create table PostOggetto (
     id serial primary key,
@@ -23,7 +49,7 @@ create table PostOggetto (
     ha_feedback boolean not null,
     voto Voto, 
     commento StringaM,
-    istante timestamp,
+    istante_feedback timestamp,
     categoria StringaM not null,
     -- vincoli di ennupla per modellare [V.PostOggetto.feedback]
     check(
@@ -36,16 +62,20 @@ create table PostOggetto (
 
     -- v.incl. (id) occorre in met_post(postoggetto)
 
-    -- completare con 'pubblica'
+    foreign key (pubblica) references Utente(username)
 );
 
-create OggettoNuovo (
-    postoggetto integer primary key,
+create table PostOggettoNuovo (
+    postoggetto PosInteger primary key,
     pubblica_nuovo StringaM not null,
     anni_garanzia IntGZ1 not null, 
-    foreign key (postoggetto, pubblica_nuovo) references postoggetto(id, pubblica),
+    foreign key (postoggetto, pubblica_nuovo) references PostOggetto(id, pubblica),
     foreign key (pubblica_nuovo)references VenditoreProfessionale(utente)
-)
+);
+
+create table Metodopagamento(
+    nome StringaM primary key
+);
 
 create table met_post (
     postoggetto integer not null,
@@ -55,20 +85,8 @@ create table met_post (
     foreign key (metodo) references Metodopagamento(nome)
 );
 
-create table Privato (
-    utente StringaM primary key,
-    foreign key (utente) refernces Utente(username)
-);
-
-create table VenditoreProfessionale (
-    utente StringaM primary key,
-    vetrina URL not null,
-    unique(vetrina)
-    foreign key (utente) refernces Utente(username)
-);
-
-
--- [V.utente.compl] e [V.Utente.disj] non ancora implementati 
 
 
 
+
+commit; 
