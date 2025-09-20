43e29b994ece28ce4075690d374b34a238dc7c10
malaonda222 <bandinelli.lisa@gmail.com>
2025-09-20 13:28:36 +0200

    commit

--- Esempi/eBuy/eBuy.sql
+++ Esempi/eBuy/eBuy.sql
@@ -4,89 +4,125 @@ set constraints all deferred;
 
 create domain StringaM as varchar(100);
 create domain Voto as integer (value between 0 and 5);
-create domain IntGZ1 as integer check (value > 1);
+create domain IntGEZ as integer check (value >= 0);
+create domain IntG1 as integer check (value > 1);
 create domain URL as varchar check (value ~ "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$");
 create domain RealGEZ as real check (value >= 0);
 create domain RealGZ as real check (value > 0);
-create domain PosInteger as integer check (value >= 0);
 create type Condizione as enum ('ottimo', 'buono', 'discreto', 'da sistemare');
 create type Popolarita as enum ('Bassa', 'Media', 'Alta');
 
 
-create table Categoria (
-    nome StringaM primary key,
-    super StringaM,
-    check (nome <> super)
-); 
-
-alter table Categoria add foreign key (super) references Categoria(nome)
-
 create table Utente (
-    username StringaM primary key, 
+    username StringaM not null,
     registrazione timestamp not null,
-);
-
-create table Privato (
-    utente StringaM primary key,
-    foreign key (utente) references Utente(username)
+    username primary key
 );
 
 create table VenditoreProfessionale (
-    utente StringaM primary key,
+    utente StringaM not null,
     vetrina URL not null,
-    unique(vetrina)
+    unique (vetrina)
+    primary key (utente),
+    foreign key (utente) references Utente(username)
+);
+
+create table Privato (
+    utente StringaM not null,
+    primary key (utente)
     foreign key (utente) references Utente(username)
 );
 
 
--- [V.utente.compl] e [V.Utente.disj] non ancora implementati 
-
 create table PostOggetto (
-    id serial primary key,
-    pubblica StringaM not null,
+    id serial not null,
     descrizione StringaM not null,
     pubblicazione timestamp not null,
     ha_feedback boolean not null,
-    voto Voto, 
+    voto Voto,
+    istante_commento timestamp,
     commento StringaM,
-    istante_feedback timestamp,
-    categoria StringaM not null,
-    -- vincoli di ennupla per modellare [V.PostOggetto.feedback]
-    check(
-        (ha_feedback = true)
+    primary key (id),
+    check (
+        ha_feedback = true
         =
-        (voto is not null and istante is not null)
-        ),
-    check(commento is null or ha_feedback = true)
-    foreign key(categoria) references Categoria(nome)
+        voto is not null AND istante_commento is not null
+        )
+    check (commento is null OR ha_feedback = true)
 
-    -- v.incl. (id) occorre in met_post(postoggetto)
+    -- v.inclusione: [V.inclusione PostOggetto(id) occorre in pubblica(postoggetto)]
+    foreign key Postoggetto(id) references pubblica(postoggetto)
+    -- v.inclusione: [V.inclusione PostOggetto(id) occorre in met_post(postoggetto)]
 
-    foreign key (pubblica) references Utente(username)
+    -- v.inclusione: [V.inclusione PostOggetto(id) occorre in cat_post(postoggetto)]
+    foreign key Postoggetto(id) references cat_post(postoggetto)
+); 
+
+
+create table OggettoUsato (
+    postoggetto IntGEZ not null, 
+    condizione Condizione not null,
+    anni_garanzia IntGEZ not null,
+    primary key (postoggetto),
+    foreign key (postoggetto) references PostOggetto(id)
 );
+
 
 create table PostOggettoNuovo (
-    postoggetto PosInteger primary key,
-    pubblica_nuovo StringaM not null,
-    anni_garanzia IntGZ1 not null, 
-    foreign key (postoggetto, pubblica_nuovo) references PostOggetto(id, pubblica),
-    foreign key (pubblica_nuovo)references VenditoreProfessionale(utente)
-);
-
-create table Metodopagamento(
-    nome StringaM primary key
-);
-
-create table met_post (
-    postoggetto integer not null,
-    metodo StringaM not null, 
-    primary key (postoggetto, metodo),
-    foreign key (postoggetto) references Postoggetto(id)
-    foreign key (metodo) references Metodopagamento(nome)
+    postoggetto IntGEZ not null,
+    anni_garanzia IntG1 not null,
+    primary key (postoggetto),
+    foreign key (postoggetto) references PostOggetto(id),
+    foreign key (postoggetto) references pubblica_nuovo(postoggettonuovo)
 );
 
 
+create table pubblica_nuovo (
+    postoggettonuovo IntGEZ not null,
+    venditoreprofessionale StringaM not null,
+    primary key (postoggettonuovo),
+    foreign key (postoggettonuovo) references PostOggettoNuovo(postoggetto),
+    foreign key (venditoreprofessionale) references VenditoreProfessionale(utente)
+);
 
 
+create table pubblica (
+    utente StringaM not null,
+    postoggetto IntGEZ not null,
+    primary key (postoggetto),
+    foreign key (postoggetto) references PostOggetto(id),
+    foreign key (utente) references Utente(username)
+);
 
-commit; 
+
+create table MetodoDiPagamento (
+    nome StringaM not null,
+    primary key nome
+)
+
+
+create table met_post ( 
+    metodo StringaM not null,
+    postoggetto IntGEZ not null,
+    primary key (metodo, postoggetto),
+    foreign key (metodo) references MetodoDiPagamento(nome),
+    foreign key (postoggetto) references PostOggetto(id)
+);
+
+
+create table Categoria (
+    nome StringaM not null, 
+    super StringaM, 
+    check (nome <> super)
+);
+
+alter table add foreign key (super) references Categoria(nome)
+
+
+create table cat_post (
+    categoria StringaM not null,
+    postoggetto IntGEZ not null,
+    primary key (postoggetto),
+    foreign key (categoria) references Categoria(nome),
+    foreign key (postoggetto) references PostOggetto(id)
+);
